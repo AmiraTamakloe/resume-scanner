d8b4baf7b3624fb3dca4669b6e42480d5ddab331
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-08-10 09:49:24 -0400

    beginning of cleaning up model training for flask app

---
+++ model/data-preprocessing.py
@@ -0,0 +1,18 @@
+from PyPDF2 import PdfReader
+
+import re
+
+def cleanDocument(resumeText):
+    resumeText = re.sub('http\S+\s*', ' ', resumeText)  # remove URLs
+    resumeText = re.sub('@\S+', '  ', resumeText)  # remove mentions
+    resumeText = re.sub('#\S+\s', ' ', resumeText)  # remove hashtags
+    resumeText = re.sub('RT|cc', ' ', resumeText)  # remove RT and cc
+    resumeText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', resumeText)  # remove punctuations
+    resumeText = re.sub(r'[^\x00-\x7f]',' ', resumeText) #remove non-ascii characters
+    resumeText = re.sub('\s+', ' ', resumeText)  # remove extra whitespace
+    return resumeText.lower()
+
+
+def transformPdfToText(file):
+    cv_text = PdfReader(file).pages[0].extract_text().lower()
+    return cv_text
